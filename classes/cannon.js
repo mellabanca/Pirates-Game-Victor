2dde2e62d2b1422ad7573e09eb2fc4a3e64a49e7
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-06-16 10:10:08 -0300

    16/06/2022

--- classes/cannon.js
+++ classes/cannon.js
@@ -20,7 +20,7 @@ class Cannon {
        translate(this.x, this.y);
        rotate(this.angle);
         imageMode(CENTER);
-        image(this.cannon_image, 0, 0, this.width, this.height);
+        image(this.cannon_image, 0, 0,this.width, this.height);
        pop();
 
        image(this.cannon_base, 70, 20, 200, 200);
